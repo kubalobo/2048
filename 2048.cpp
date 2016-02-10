9323b6c6e6fc569637ca110baa509531686d110d
Kuba <kubalobo@gmail.com>
2016-02-10 10:55:22 +0100

    board struct v1

--- 2048.cpp
+++ 2048.cpp
@@ -7,6 +7,9 @@
 //============================================================================
 
 #include <iostream>
+
+#include "board.h"
+
 using namespace std;
 
 int main() {
---
+++ board.h
@@ -0,0 +1,22 @@
+/*
+ * board.h
+ *
+ *  Created on: 10 lut 2016
+ *      Author: kubalobo
+ */
+
+#ifndef BOARD_H_
+#define BOARD_H_
+
+#include <vector>
+
+struct board {
+
+	std::vector<int> row[5];
+	std::vector<int> column[5];
+
+};
+
+
+
+#endif /* BOARD_H_ */
