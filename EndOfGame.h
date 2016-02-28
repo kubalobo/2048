b3753aa01e6b686d74abbdd593b9985b9a35c69a
Kuba <kubalobo@gmail.com>
2016-02-29 04:27:17 +0800

    bool EndOfGame

--- EndOfGame.h
+++ EndOfGame.h
@@ -10,25 +10,31 @@
 
 //sprawdzenie czy koniec gry
 
-void EndOfGame(board plansza) {
+bool EndOfGame(board plansza) {
 
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
-			if (plansza.field[i][j] != plansza.field[i + 1][j]
-					&& plansza.field[i][j] != 0 && plansza.field[i+1][j] != 0) {
-				std::cout << "END OF GAME!!!";
+			if (plansza.field[i][j] != plansza.field[i + 1][j] && 
+				plansza.field[i][j] != 0 && 
+				plansza.field[i+1][j] != 0) {
+				return true;
+				//std::cout << "END OF GAME!!!";
 			}
 		}
 	}
 
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
-			if (plansza.field[i][j] != plansza.field[i][j + 1]
-					&& plansza.field[i][j] != 0 && plansza.field[i][j + 1] != 0) {
-				std::cout << "END OF GAME!!!";
+			if (plansza.field[i][j] != plansza.field[i][j + 1] && 
+				plansza.field[i][j] != 0 && 
+				plansza.field[i][j + 1] != 0) {
+				return true;
+				//std::cout << "END OF GAME!!!";
 			}
 		}
 	}
+
+	return false;
 }
 
 
