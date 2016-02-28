8e0fd7f4bab5da6032648ab58b97f795d00a349a
monyka <kwiatkowska_m@yahoo.com>
2016-02-28 18:16:08 +0100

    próba naprawienia funkcji newElementsGenerator

--- newElementsGenerator.h
+++ newElementsGenerator.h
@@ -25,16 +25,23 @@ int new_element()
 }
 
 //Wstawia nowy element w losowe miejsce na planszy | jesli zajete to rekurencyjnie powtarza az trafi - B£¥D!
-void newElementsGenerator(board& plansza)
-{
+void newElementsGenerator(board& plansza) {
 
-	int a =(rand() %  4) + 0;
-	int b =(rand() %  4) + 0;
-	if (plansza.field[a][b]==0)
-		plansza.field[a][b]= new_element();
-	else 
-		newElementsGenerator(plansza);
+	for (int i = 0; i < 4; i++)
+		for (int j = 0; j < 4; j++) {
+			if (plansza.field[i][j] != 0)
+				break;
+			else {
+				int a = (rand() % 4) + 0;
+				int b = (rand() % 4) + 0;
 
+				if (plansza.field[a][b] == 0)
+					plansza.field[a][b] = new_element();
+				else
+					newElementsGenerator(plansza);
+			}
+		}
 }
+
 
 
