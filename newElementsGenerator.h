e4837223b3a914acf06860d34f477fce9edd8fbb
Kuba <kubalobo@gmail.com>
2016-02-29 04:58:13 +0800

    neElement FIX

--- newElementsGenerator.h
+++ newElementsGenerator.h
@@ -25,26 +25,34 @@ int new_element()
 }
 
 //Wstawia nowy element w losowe miejsce na planszy | jesli zajete to rekurencyjnie powtarza az trafi - B£¥D!
-/*void newElementsGenerator(board& plansza) {
-
+void newElementsGenerator(board& plansza) {
+	
+	int zera = 0;
+	
 	for (int i = 0; i < 4; i++)
-		for (int j = 0; j < 4; j++) {
-			if (plansza.field[i][j] != 0)
-				break;
-			else {
-				int a = (rand() % 4) + 0;
-				int b = (rand() % 4) + 0;
+		for (int j = 0; j < 4; j++) 
+			if (plansza.field[i][j] == 0)
+				zera++;
 
-				if (plansza.field[a][b] == 0)
-					plansza.field[a][b] = new_element();
-				else
-					newElementsGenerator(plansza);
-			}
-		}
-}*/
+	if (zera == 0)
+	{
+		//SprawdŸ, czy: EndOfGame(plansza);
+	}
+	else 
+	{
+		int a = (rand() % 4) + 0;
+		int b = (rand() % 4) + 0;
+
+		if (plansza.field[a][b] == 0)
+			plansza.field[a][b] = new_element();
+		else
+			newElementsGenerator(plansza);
+	}
+		
+}
 
 
-
+/*
 void newElementsGenerator(board& plansza)
 {
 
@@ -58,4 +66,4 @@ void newElementsGenerator(board& plansza)
 }
 
 
-
+*/
